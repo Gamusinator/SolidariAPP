a39e4a01160e903c1c3555df6a0edf72882a39b2
Gamusinator <danicano1212@gmail.com>
2018-02-16 00:01:37 +0100

    15/02/2018 - nit -

--- app/src/main/java/gamusinostudios/solidariapp/Fragment01.java
+++ app/src/main/java/gamusinostudios/solidariapp/Fragment01.java
@@ -1,29 +1,18 @@
 package gamusinostudios.solidariapp;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.firebase.auth.AuthCredential;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /*
@@ -34,38 +23,35 @@ import com.google.firebase.auth.AuthCredential;
  * Use the {@link Fragment01#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment01 extends Fragment implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
+public class Fragment01 extends Fragment implements View.OnClickListener{
 
-    View vista;
     private LinearLayout Prof_Section;
-    private Button SignOut;
-    private SignInButton SignIn;
     private TextView Name, Email;
     private ImageView Prof_Pic;
-    private GoogleApiClient googleApiClient;
-    private static final int REQ_CODE = 9001;
+    private boolean isLogin;
+    String name;
+    String email;
+    String img_url;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment01, container, false);
-        vista = v;
 
         Prof_Section = v.findViewById(R.id.perfil);
-        SignOut = v.findViewById(R.id.btnLogOff);
-        SignIn = v.findViewById(R.id.btnLogOn);
         Name = v.findViewById(R.id.nomPerfil);
         Email = v.findViewById(R.id.correuPerfil);
         Prof_Pic = v.findViewById(R.id.fotoPerfil);
 
-        SignIn.setOnClickListener(this);
-        SignOut.setOnClickListener(this);
+        SharedPreferences prefs = getActivity().getSharedPreferences("SolidariAPP", MODE_PRIVATE);
+        name = prefs.getString("name", null);
+        email = prefs.getString("email", null);
+        img_url = prefs.getString("pic", null);
+        isLogin = prefs.getBoolean("login", false);
 
-        //amaguem el Layout amb les dades de l'usuari mentre no estigui connectat.
-        Prof_Section.setVisibility(v.GONE);
+        Prof_Section.setVisibility(View.GONE);
 
-
-
+        update(isLogin);
         // Inflate the layout for this fragment
         return v;
     }
@@ -75,12 +61,6 @@ public class Fragment01 extends Fragment implements View.OnClickListener, Google
 
         switch (vi.getId())
         {
-            case R.id.btnLogOn:
-                signIn();
-                break;
-            case R.id.btnLogOff:
-                signOut();
-                break;
             case R.id.btnPubli:
                 //obrir publicitat
                 break;
@@ -89,68 +69,20 @@ public class Fragment01 extends Fragment implements View.OnClickListener, Google
         }
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    private void signIn(){
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleApiClient = new GoogleApiClient.Builder(getContext()).enableAutoManage(getActivity(),this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
-        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(intent, REQ_CODE);
-    }
-
-    private void signOut(){
-        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                update(false);
-            }
-        });
-    }
-
-    private void handleResult(GoogleSignInResult result){
-
-        if (result.isSuccess()){
-            GoogleSignInAccount account = result.getSignInAccount();
-            String name = account.getDisplayName();
-            String email = account.getEmail();
-            if (account.getPhotoUrl() != null){
-                String img_url=account.getPhotoUrl().toString();
-                Glide.with(this).load(img_url).into(Prof_Pic);
-            }else{
-                Prof_Pic.setImageResource(R.mipmap.ic_launcher);
-            }
-            Name.setText(name);
-            Email.setText(email);
-
-            update(true);
-        }
-        else{
-            update(false);
-        }
-    }
 
     private void update(boolean isLogin){
 
         if (isLogin){
-            Prof_Section.setVisibility(vista.VISIBLE);
-            SignIn.setVisibility(vista.GONE);
+            Prof_Section.setVisibility(View.VISIBLE);
+            Name.setText(name);
+            Email.setText(email);
+            if (img_url == null){
+                Prof_Pic.setImageResource(R.mipmap.ic_launcher_round);
+            }else {
+                Glide.with(this).load(img_url).into(Prof_Pic);
+            }
         }else{
-            Prof_Section.setVisibility(vista.GONE);
-            SignIn.setVisibility(vista.VISIBLE);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode ==REQ_CODE)
-        {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleResult(result);
+            Prof_Section.setVisibility(View.GONE);
         }
     }
 }
--- app/src/main/java/gamusinostudios/solidariapp/MainActivity.java
+++ app/src/main/java/gamusinostudios/solidariapp/MainActivity.java
@@ -1,22 +1,33 @@
 package gamusinostudios.solidariapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.support.v4.app.FragmentManager;
+import android.widget.Toast;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
-    private static final int RC_SIGN_IN = 9001;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
 
     FragmentManager fragmentManager=getSupportFragmentManager();
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -61,8 +72,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
 
+        signIn();
+    }
 
     public void CompartirAPP() {
         Intent intent = new Intent(Intent.ACTION_SEND);
@@ -75,4 +89,53 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("MIAPP", connectionResult.getErrorMessage());
+    }
+
+    private void signIn(){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent, REQ_CODE);
+    }
+
+    private void handleResult(GoogleSignInResult result){
+
+        SharedPreferences.Editor editor = getSharedPreferences("SolidariAPP", MODE_PRIVATE).edit();
+        if (result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            String img_url;
+            if (account.getPhotoUrl() != null){
+                img_url=account.getPhotoUrl().toString();
+
+            }else{
+                img_url ="";
+            }
+            editor.putBoolean("login", true);
+            editor.putString("name", name);
+            editor.putString("email", email);
+            editor.putString("pic", img_url);
+            editor.apply();
+            Toast.makeText(this, "Dades de perfil carregades correctament", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "No s'ha pogut accedir al prefil de Google", Toast.LENGTH_SHORT).show();
+            editor.putBoolean("login", false);
+            editor.apply();
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode ==REQ_CODE)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
+    }
 }
